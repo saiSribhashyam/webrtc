3724734ac4d311b80374c571a39fb5c9b717e7a9
saiSribhashyam <110450908+saiSribhashyam@users.noreply.github.com>
2024-07-08 19:57:39 +0530

    Minor Changes

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ let createPeerConnection = async (MemberId) => {
 
 
     if(!localStream){
-        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
         document.getElementById('user-1').srcObject = localStream
     }
 
